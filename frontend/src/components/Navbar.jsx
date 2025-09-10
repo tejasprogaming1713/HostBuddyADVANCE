1b729a9c9303d3ec29ee43306e71b821dc7d6ebc
tejasprogaming1713 <tejassgupta1327@gmail.com>
2025-09-10 15:32:52 +0530

    Create Navbar.jsx

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="bg-gray-950 p-4 shadow-md">
+      <h1 className="text-xl font-bold">⚡ VPS Manager</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
