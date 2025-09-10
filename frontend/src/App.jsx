a88577f32fa03b79cb63ca1e8f357378071dbfb7
tejasprogaming1713 <tejassgupta1327@gmail.com>
2025-09-10 15:21:56 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
